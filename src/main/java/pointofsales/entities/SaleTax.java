4e8fc65cab97ba7310e8638adcc0f12c55205137
TJ Cappelletti <tuc156@psu.edu>
2024-10-15 11:05:39 -0400

    Create SaleTax class

---
+++ src/main/java/pointofsales/entities/SaleTax.java
@@ -0,0 +1,53 @@
+package pointofsales.entities;
+
+public class SaleTax {
+    private Double amount;
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    private AmountType amountType;
+
+    public AmountType getAmountType() {
+        return amountType;
+    }
+
+    public void setAmountType(AmountType amountType) {
+        this.amountType = amountType;
+    }
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
