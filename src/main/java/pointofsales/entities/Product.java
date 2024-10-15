e1f04fc69932351091231d383cbae5e76592d9fd
TJ Cappelletti <tuc156@psu.edu>
2024-10-15 11:05:46 -0400

    Created Product class

---
+++ src/main/java/pointofsales/entities/Product.java
@@ -0,0 +1,75 @@
+package pointofsales.entities;
+
+import java.util.List;
+
+public class Product {
+    private String category;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private List<Note> notes;
+
+    public List<Note> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(List<Note> notes) {
+        this.notes = notes;
+    }
+
+    private Double price;
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    private List<SaleTax> saleTaxes;
+
+    public List<SaleTax> getSaleTaxes() {
+        return saleTaxes;
+    }
+
+    public void setSaleTaxes(List<SaleTax> saleTaxes) {
+        this.saleTaxes = saleTaxes;
+    }
+}
